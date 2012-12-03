4e6e5c3017d66046a99cba90f8eb8bbf96564600
Student <student@Habitat.(none)>
2012-12-02 19:26:00 -0500

    Dec 2 commit

---
+++ js/mouseover.js
@@ -0,0 +1,21 @@
+   
+
+// create function to get left image on roll over
+
+function doMouseOver() {
+	
+	var img = document.getElementById('image2');
+	img.src = "images/evilHomer_L.gif";
+	
+}
+
+// create function to get right image on mouse out
+
+function doMouseOut() {
+	
+	var img = document.getElementById('image2');
+	img.src = "images/evilHomer_R.gif";
+	
+}
+
+
